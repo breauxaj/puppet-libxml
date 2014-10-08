ec23fffc1c3643fbc09e195d7365b28252b4fe21
Adam Breaux <breauxaj@gmail.com>
2014-10-08 08:14:38 -0400

    standard preamble
    permit selecting state of package

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,39 @@
-class libxml {
+# Class: libxml
+#
+# This class installs the libxml2 package
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the libxml2 package
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'libxml2':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'libxml2':
+#      ensure => 'absent'
+#    }
+#
+class libxml (
+  $ensure = 'latest'
+){
   $required = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'libxml2' ],
   }
 
-  package { $required: ensure => latest }
+  package { $required: ensure => $ensure }
 
   file { '/usr/lib64/libxml2.so':
     ensure => 'link',
